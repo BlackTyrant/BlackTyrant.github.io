0686cab9773620c940f01119c1d2106e1af6d9f7
BlackTyrant <gilang.dani97@gmail.com>
2017-06-20 08:58:38 +0700

    Add files via upload

---
+++ UAS/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "7 + 5 = ?",
+	"option1": "12",
+	"option2": "7",
+	"option3": "5",
+	"option4": "2",
+	"answer": "1"
+}, {
+	"question": "Doni mempunya 30 permen, dan Susi meminta 20 permen. Berapakah sisa permen Doni?",
+	"option1": "30",
+	"option2": "50",
+	"option3": "10",
+	"option4": "31",
+	"answer": "3"
+}, {
+	"question": "9 + 3 - 4 = ?",
+	"option1": "1",
+	"option2": "2",
+	"option3": "16",
+	"option4": "8",
+	"answer": "4"
+}, {
+	"question": "Jarak Jogja ke Solo 70 km dan Devi sudah menempuh 45 km. Berapa jarak yang harus ditempuh Devi ?",
+	"option1": "105 km",
+	"option2": "25 km",
+	"option3": "50 km",
+	"option4": "30 km",
+	"answer": "2"
+}, {
+	"question": "30 - 15 = ?",
+	"option1": "15",
+	"option2": "45",
+	"option3": "12",
+	"option4": "10",
+	"answer": "1"
+}, {
+	"question": "15 - 10 + 3 = ?",
+	"option1": "8",
+	"option2": "9",
+	"option3": "28",
+	"option4": "30",
+	"answer": "1"
+}, {
+	"question": "Sebuah ember dapat menampung 5 liter air jika sebelumnya ember telah berisi 3 liter air, berapa liter diperlukan untuk memenuhi ember ?",
+	"option1": "1",
+	"option2": "2",
+	"option3": "3",
+	"option4": "4",
+	"answer": "2"
+}, {
+	"question": "4 + 7 = ?",
+	"option1": "10",
+	"option2": "9",
+	"option3": "12",
+	"option4": "11",
+	"answer": "4"
+}, {
+	"question": "11 - 5 - 2 = ?",
+	"option1": "4",
+	"option2": "5",
+	"option3": "6",
+	"option4": "7",
+	"answer": "1"
+}, {
+	"question": "Dalam sebuah toples terdapat 30 batang wafer, kemudian Rani menambahkan 15 batang kedalamnya, tidak berselang lama Roni mengambil 10 batang wafer dari dalam toples. Berapakah wafer di dalam toples?",
+	"option1": "50",
+	"option2": "40",
+	"option3": "20",
+	"option4": "35",
+	"answer": "4"
+}];
